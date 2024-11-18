83a3a6892ee4a4e1ff8b40ae98d843eb7ff18136
Adrian Gogacz <adrian.gogacz.external@worldline.com>
2024-11-18 16:39:07 +0100

    add data model

---
+++ Main/ToDoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Main;
+
+public class ToDoItem
+{
+    public string TaskName { get; set; }
+    public DateTime DeadLine { get; set; }
+    public string NotificationEmail { get; set; }
+    public bool Done { get; set; }
+}
