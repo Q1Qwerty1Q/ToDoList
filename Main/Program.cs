0207e580216cfed60b1c31bf4a276fa22a888c4f
Cywka Sandra <Cywka.Sandra@SZKOLA.ZST-RADOM.EDU.PL>
2024-11-25 15:47:01 +0100

    TaskListMenu

--- Main/Program.cs
+++ Main/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Main;
+using System.Diagnostics;
+
+internal class Program
+{
+
+   
+    
+
+    private static void Main(string[] args)
+    {
+
+        bool running = true;
+
+        while(running)
+        {
+            Console.Clear();
+            Console.WriteLine("=== Lista Zadań ===");
+            Console.WriteLine("=== 1. Wyświetl zadania ===");
+            Console.WriteLine("=== 2. Dodaj zadanie ===");
+            Console.WriteLine("=== 3. Usuń zadanie ===");
+            Console.WriteLine("=== 4. Wyjdź ===");
+
+
+
+        }
+
+        string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "1":
+                displayTask.DisplayTask();
+                break;
+            case "2":
+                displayTask.AddTask();
+                break;
+            case "3":
+                displayTask.RemoveTask();
+                break;
+            case "4":
+                running = false;
+                break;
+            default:
+                Console.WriteLine("Nieprawidłowa opcja, spróbuj jeszcze raz.");
+                Console.ReadKey();
+                break;
+        }
+    }
+
+    
+
+    
+   
+
+  
+
+
+   
+
+}
